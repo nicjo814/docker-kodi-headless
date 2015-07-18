2db94098d0e11a8cc1c2e200c2141e756d371dc8
Niclas Berglind <nb@kjam.se>
2015-07-18 21:53:12 +0200

    Rewritten addon install mgmt (without xam).

--- src/addon_install.php
+++ src/addon_install.php
@@ -1,5 +1,41 @@
 #!/usr/bin/php -q
 <?php
+require_once("repos.php");
+
+function getAllAddons($repos) {
+	$allAddons=array();
+	foreach($repos as $repo) {	
+		$xmlString=file_get_contents($repo['xmlURL']);
+		$xmlObj=simplexml_load_string($xmlString);
+		foreach($xmlObj as $addon) {
+			if(isset($allAddons[(string)$addon->attributes()->{'id'}])) {
+				/*
+				print("Addon " . (string)$addon->attributes()->{'id'} . " already found in repo " .
+					$allAddons[(string)$addon->attributes()->{'id'}]["repo"] . " with version " . 
+					$allAddons[(string)$addon->attributes()->{'id'}]["version"] . "\n" .
+					"Now also found in repo " . $repo["name"] . " with version " . (string)$addon->attributes()->{'version'} . "\n\n\n");
+				 */
+				continue;
+			} else {
+				$dependencies=array();
+				if(isset($addon->requires->import)) {
+					foreach($addon->requires->import as $req) {
+						array_push($dependencies, array(
+							"name"=>(string)$req->attributes()->{'addon'},
+							"version"=>(string)$req->attributes()->{'version'}));
+					}
+				}
+				$allAddons[(string)$addon->attributes()->{'id'}] = array(
+					"version"=>(string)$addon->attributes()->{'version'},
+					"repo"=>$repo["name"],
+					"dlURL"=>$repo["repoURL"] . "/" . (string)$addon->attributes()->{'id'} . "/" .
+					(string)$addon->attributes()->{'id'} . "-" . (string)$addon->attributes()->{'version'} . ".zip",
+						"dependencies"=>$dependencies);
+			}
+		}
+	}
+	return $allAddons;
+}
 
 function get_dependencies($addon) {
 	$kodipath="/opt/kodi-server/share/kodi/portable_data/addons";
@@ -11,43 +47,30 @@ function get_dependencies($addon) {
 	return $dependencies;
 }
 
-function get_addon($addon, $repos) {
+function installAddon($addon, $allAddons) {
 	print("Start searching for addon $addon...\n");
 	$kodipath="/opt/kodi-server/share/kodi/portable_data/addons";
-	$found=false;
-	for ($i=count($repos)-1;$i>=0;$i--) {
-		unset($out);
-		$cmd="xam get --repo $repos[$i] $addon 2>&1";
-		exec($cmd, $out, $res);
-		foreach($out as $line) {
-			print("$line\n");
-			if(preg_match('/^Downloading.*$/', $line)) {
-				$found=true;
-				print("Addon $addon installed\n\n\n");
-				break;
-			}
+	if(!isset($allAddons[$addon])) {
+		print("NOTICE: Addon $addon not found.\n");
+	} else {
+		print("Downloading addon $addon from " . $allAddons[$addon]['repo'] . " repo...\n");
+		$cmd="wget " . $allAddons[$addon]['dlURL'] . " -O " . $addon . "-" . $allAddons[$addon]['version'] . ".zip";
+		exec($cmd);
+		print("Installing addon...\n");
+		$cmd="mkdir -p \"" . $kodipath . "\""; 
+		exec($cmd);
+		$cmd="unzip -o \"" . $addon . "-" . $allAddons[$addon]['version'] . ".zip\" -d \"" . $kodipath . "\"";
+		exec($cmd);
+		print("Addon $addon successfully installed.\n\n\n");
+		foreach($allAddons[$addon]['dependencies'] as $dep) {
+			installAddon($dep['name'], $allAddons);
 		}
-		if($found) {
-			exec("mkdir -p '$kodipath'");
-			exec("unzip -o '$addon/*.zip' -d'$kodipath'");
-			$dependencies=get_dependencies($addon);
-			foreach($dependencies as $dep) {
-				get_addon($dep, $repos);
-			}
-			break;
-		}
-	}
-	if(!$found) {
-		print("Addon $addon not found!\n\n\n");
 	}
 }
 
 $addons=explode("|",$argv[1]);
-exec("xam all --repo 2>&1", $out);
-$prestr=preg_quote("usage: xam all [-h] [--repo {");
-preg_match("/^$prestr([A-Z\,]+)\}\]$/", $out[0], $matches);
-$repos=explode(",",$matches[1]);
+$allAddons=getAllAddons($repos);
 foreach($addons as $addon) {
-	get_addon($addon, $repos);
+	installAddon($addon, $allAddons);
 }
 ?>
---
+++ src/repos.php
@@ -0,0 +1,33 @@
+<?php
+$repos=array(
+	array(
+		"name"=>"Isengard",
+		"xmlURL"=>"http://mirrors.xbmc.org/addons/isengard/addons.xml",
+		"repoURL"=>"http://mirrors.xbmc.org/addons/isengard",
+		"md5URL"=>"http://mirrors.xbmc.org/addons/isengard/addons.xml.md5"),
+	array(
+		"name"=>"Helix",
+		"xmlURL"=>"http://mirrors.xbmc.org/addons/helix/addons.xml",
+		"repoURL"=>"http://mirrors.xbmc.org/addons/helix",
+		"md5URL"=>"http://mirrors.xbmc.org/addons/helix/addons.xml.md5"),
+	array(
+		"name"=>"Gotham",
+		"xmlURL"=>"http://mirrors.xbmc.org/addons/gotham/addons.xml",
+		"repoURL"=>"http://mirrors.xbmc.org/addons/gotham",
+		"md5URL"=>"http://mirrors.xbmc.org/addons/gotham/addons.xml.md5"),
+	array(
+		"name"=>"Frodo",
+		"xmlURL"=>"http://mirrors.xbmc.org/addons/frodo/addons.xml",
+		"repoURL"=>"http://mirrors.xbmc.org/addons/frodo",
+		"md5URL"=>"http://mirrors.xbmc.org/addons/frodo/addons.xml.md5"),
+	array(
+		"name"=>"Eden",
+		"xmlURL"=>"http://mirrors.xbmc.org/addons/eden/addons.xml",
+		"repoURL"=>"http://mirrors.xbmc.org/addons/eden",
+		"md5URL"=>"http://mirrors.xbmc.org/addons/eden/addons.xml.md5"),
+	array(
+		"name"=>"Dharma",
+		"xmlURL"=>"http://mirrors.xbmc.org/addons/dharma/addons.xml",
+		"repoURL"=>"http://mirrors.xbmc.org/addons/dharma",
+		"md5URL"=>"http://mirrors.xbmc.org/addons/dharma/addons.xml.md5"));
+?>
